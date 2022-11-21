dda1187071275e4bfa652e5ef0c96aac94f153c3
felipenmatos <notfelipe2@hotmail.com>
2022-11-21 00:52:25 -0300

    feat: database connection and export of values

---
+++ data.js
@@ -0,0 +1,25 @@
+const dotenv = require("dotenv");
+dotenv.config();
+const MONGO_CNSTRING = process.env.MONGO_CNSTRING;
+const { MongoClient } = require("mongodb");
+const dbName = "myProject";
+const client = new MongoClient(MONGO_CNSTRING);
+
+function baseData() {
+  const result = main();
+  return result;
+}
+
+var dados = [];
+
+async function main() {
+  await client.connect();
+  const db = client.db(dbName);
+  const testCollection = db.collection("testcollection");
+  const findResult = await testCollection.find({}).toArray();
+  return findResult;
+}
+
+main().then((res) => dados.push(res));
+
+module.exports = dados;
