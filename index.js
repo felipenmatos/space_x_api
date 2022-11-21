753d035560e6d3311e56ec722fea7dd0fdfc43f7
felipenmatos <notfelipe2@hotmail.com>
2022-11-20 23:11:49 -0300

    feat: installation of dependencies and connection to the mongodb database

--- index.js
+++ index.js
@@ -5,3 +5,27 @@ const app = express();
 app.use(express.json());
 
 app.listen(8000);
+
+const dotenv = require("dotenv");
+dotenv.config();
+
+const MONGO_CNSTRING = process.env.MONGO_CNSTRING;
+
+const { MongoClient } = require("mongodb");
+
+// Database Name
+const dbName = "myProject";
+
+const client = new MongoClient(MONGO_CNSTRING);
+
+client.connect(async function (err) {
+  const db = client.db(dbName);
+  const testCollection = db.collection("testcollection");
+  await testCollection.insertOne({
+    test: true,
+  });
+  client.close();
+
+  const findResult = await testCollection.find({}).toArray();
+  return findResult;
+});
