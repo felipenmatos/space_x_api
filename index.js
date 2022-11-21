50fb72e1d00528a5f6d53a30db7c968c2e4b7f0a
felipenmatos <notfelipe2@hotmail.com>
2022-11-21 00:54:30 -0300

    feat: connection to router on server

--- index.js
+++ index.js
@@ -1,31 +1,9 @@
 const express = require("express");
+const roteador = require("./roteador/roteador");
 
 const app = express();
 
 app.use(express.json());
+app.use(roteador);
 
 app.listen(8000);
-
-const dotenv = require("dotenv");
-dotenv.config();
-
-const MONGO_CNSTRING = process.env.MONGO_CNSTRING;
-
-const { MongoClient } = require("mongodb");
-
-// Database Name
-const dbName = "myProject";
-
-const client = new MongoClient(MONGO_CNSTRING);
-
-client.connect(async function (err) {
-  const db = client.db(dbName);
-  const testCollection = db.collection("testcollection");
-  await testCollection.insertOne({
-    test: true,
-  });
-  client.close();
-
-  const findResult = await testCollection.find({}).toArray();
-  return findResult;
-});
