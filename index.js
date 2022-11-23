43ecc71ee2a462d1e19be16ae748722d59b76ab7
felipenmatos <notfelipe2@hotmail.com>
2022-11-22 21:35:28 -0300

    feat: implementation of cors on the server

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const express = require("express");
 const roteador = require("./roteador/roteador");
 
 const app = express();
+const port = process.env.PORT || 8000;
 
+app.use(cors({ origin: port }));
 app.use(express.json());
 app.use(roteador);
 
