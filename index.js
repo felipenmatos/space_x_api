d1366fc5956542fb1ac42d13f01c4ec260e329f9
felipenmatos <notfelipe2@hotmail.com>
2022-11-25 22:54:34 -0300

    feat: swagger implementation

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const cors = require("cors");
 const roteador = require("./roteador/roteador");
+const swaggerUi = require("swagger-ui-express");
 
 const app = express();
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 3000;
 
 app.use(cors({ origin: port }));
 app.use(express.json());
 app.use(roteador);
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(require("./swagger.json")));
 
 app.listen(port);
